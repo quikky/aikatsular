2cce67e6134214fe5d83680d3e509002d0d79b52
Quikky <765pro.gz@gmail.com>
2020-01-25 22:09:57 +0900

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AikatsuStarsComponent } from './aikatsu-stars/aikatsu-stars.component';
 import { AikatsuFriendsComponent } from './aikatsu-friends/aikatsu-friends.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { reducers, metaReducers } from './reducers';
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
